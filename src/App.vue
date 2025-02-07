227ea9eedc277bdbe1197d612e07876219b116cb
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2025-02-07 21:36:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="">
+    <h1 class="color">Vue 3 + Vite + Tailwind CSS</h1>
+    <p>Vue 3 + Vite + Tailwind CSS</p>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
